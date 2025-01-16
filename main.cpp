7bccd6a835555e15a95bf18de5b1917d5ceda4e8
Bernard Bisuna <bbisuna0@saddleback.edu>
2025-01-15 23:14:13 -0800

    Revert "Merge branch 'hello1Function'"
    This reverts commit 0745fedc6b181b8b74e930777b87324dd5986731, reversing
    changes made to b873b58de448d7bc67b1e31f486176576a05f363.

--- main.cpp
+++ main.cpp
@@ -18,3 +18,16 @@ int main()
     hello2Function(name2);
     return 0;
 }
+
+// hello1Function
+/*
+* hello1Function.cpp
+*
+*/
+#include <iostream>
+using namespace std;
+int hello1Function(string name1)
+{
+cout << "hello World " << name1 << endl;
+return 0;
+}
